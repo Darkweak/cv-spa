1b336e7dd9b59eab669d63386a9e40229fb07490
Darkweak <sylvain@les-tilleuls.coop>
2020-04-15 14:31:54 +0200

    Fix dc

---
+++ init-dc.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+services=("admin:admin." "api:api." "cache:cache." "ssr:")
+text="version: '3.4'
+services:"
+
+for k in "${services[@]}" ; do
+    key=${k%%:*}
+    value=${k#*:}
+    text+="
+  $key:
+    labels:
+      - traefik.http.middlewares.https-redirect.redirectscheme.scheme=https
+      - traefik.http.middlewares.https-redirect.redirectscheme.permanent=true
+      - traefik.http.routers.$key.rule=Host(\`${value}\${DOMAIN}\`)
+      - traefik.http.routers.$key.entrypoints=web-secure
+      - traefik.http.routers.$key.tls.certresolver=letsencrypt
+"
+done
+
+echo "$text" > ./docker-compose.override.yml
